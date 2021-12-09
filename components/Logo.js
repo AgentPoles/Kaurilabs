12112977b65225187fc074f921b6672c03ed6bd1
Agentpoles <pauloams16@gmail.com>
2021-12-09 11:17:33 -0800

    homepage

--- components/Logo.js
+++ components/Logo.js
@@ -1,5 +1,5 @@
 import { Image } from "@chakra-ui/image";
-import { Link } from "@chakra-ui/layout";
+import { Link, AspectRatio } from "@chakra-ui/layout";
 import React from "react";
 
 export default function Logo() {
@@ -14,3 +14,25 @@ export default function Logo() {
     </Link>
   );
 }
+
+const BigKauriImage = () => (
+  <Image
+    src={"/assets/logo.png"}
+    alt="Kauri"
+    w="40%"
+    maxW="150px"
+    objectFit="contain"
+  />
+);
+
+const MediumKauriImage = () => (
+  <Image
+    src={"/assets/logo.png"}
+    alt="Kauri"
+    w="30%"
+    maxW={["80px", "80px", "100px"]}
+    objectFit="contain"
+  />
+);
+
+export { BigKauriImage, MediumKauriImage };
